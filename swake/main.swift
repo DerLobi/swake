e0b4e0f68339cfe8b32c71d7ec37d78759768717
Christian Lobach <Christian.Lobach@gmail.com>
2015-05-25 17:32:41 +0200

    crudely parse command line flags

--- swake/main.swift
+++ swake/main.swift
@@ -14,24 +14,67 @@ if arguments.count > 1 {
     let execPath = arguments[0].stringByDeletingLastPathComponent
     let cwd = NSFileManager.defaultManager().currentDirectoryPath
     
+    let skeletonPath = execPath.stringByAppendingPathComponent("Skeleton.swift")
+    var swakefilePath = cwd.stringByAppendingPathComponent("Swakefile")
+
+    // parse arguments
+    
+    var numberOfArgumentsToSkip = 2 // 0 is the path, 1 is the task
+    var shouldListTasks = false
+    for i in [1,3] {
+        if i > arguments.count {
+            break;
+        }
+        let possibleFlag = arguments[i]
+            
+        if (possibleFlag == "-h" || possibleFlag == "--help") {
+            println("swake - it's kinda like rake, but in Swift!")
+            println()
+            println("Usage:")
+            println("swake task [arguments]                  # run the specified task with the given arguments")
+            println("swake --file /path/to/Swakefile task    # run the specified task and specify the path to the Swakefile")
+            println("swake --tasks                           # print all available tasks")
+            println("swake --help                            # print this help message")
+            exit(0)
+        }
+        
+        if (possibleFlag == "-f" || possibleFlag == "--file") {
+            swakefilePath = arguments[i + 1]
+            numberOfArgumentsToSkip += 2
+        }
+        
+        if (possibleFlag == "-t" || possibleFlag == "--tasks") {
+            shouldListTasks = true
+        }
+   
+    }
+
+    let skeleton = NSString(contentsOfFile: skeletonPath, encoding: NSUTF8StringEncoding, error: nil)
+    var swakefile = NSString(contentsOfFile: swakefilePath, encoding: NSUTF8StringEncoding, error: nil)
+
+    // hand the rest of the arguments over to the task
     let task = arguments[1]
     let taskArguments = arguments[2..<arguments.count].map({ "\"\($0)\"" })
-    
-    let skeletonPath = execPath.stringByAppendingPathComponent("Skeleton.swift")
-    let skeleton = NSString(contentsOfFile: skeletonPath, encoding: NSUTF8StringEncoding, error: nil)
-    let swakefilePath = cwd.stringByAppendingPathComponent("Swakefile")
-    let swakefile = NSString(contentsOfFile: swakefilePath, encoding: NSUTF8StringEncoding, error: nil)
-        
+
     if let swakefile = swakefile {
+
         var script = "#!/usr/bin/xcrun swift" + "\n"
         script += (skeleton! as String) + "\n"
         script += (swakefile as String) + "\n"
-        script += "let taskToExecute = tasks[\""
-        script += task
-        script += "\"]" + "\n"
-        script += "let taskArguments = \(taskArguments) as [String]" + "\n"
         
-        script += "taskToExecute!(taskArguments)"
+        if (shouldListTasks) {
+            script += "    println(\"Available tasks:\")" + "\n" + "\n"
+            script += "for key in sorted(tasks.keys) {" + "\n"
+            script += "    print(\"- \")" + "\n"
+            script += "    println(key)" + "\n"
+            script += "}" + "\n"
+        } else {
+            script += "let taskToExecute = tasks[\""
+            script += task
+            script += "\"]" + "\n"
+            script += "let taskArguments = \(taskArguments) as [String]" + "\n"
+            script += "taskToExecute!(taskArguments)" + "\n"
+        }
         
         let scriptPath = ".swakescript.sh"
         script.writeToFile(scriptPath, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
