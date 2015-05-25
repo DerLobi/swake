be4c217fa40e7898541606030faf3876ee470e91
Christian Lobach <Christian.Lobach@gmail.com>
2015-05-25 11:51:35 +0200

    Initial Commit

---
+++ swake/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swake
+//
+//  Created by Christian Lobach on 25/05/15.
+//  Copyright (c) 2015 Christian Lobach. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
